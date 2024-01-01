15d79a21dbef160befd3d7827d14b842fb652f11
박민규 <pmq0101@naver.com>
2024-01-02 00:58:55 +0900

    refactor: builder 추가

--- moodtracker/src/main/java/econovation/moodtracker/domain/user/User.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/user/User.java
@@ -4,14 +4,24 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
     private String userId;
     private String password;
+    @Builder
+    public User(Long id, String userId, String password) {
+        this.id = id;
+        this.userId = userId;
+        this.password = password;
+    }
 }
