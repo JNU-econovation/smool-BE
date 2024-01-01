cccb7c83cf8aa8e7f97a687f236c328869eac7b2
박민규 <pmq0101@naver.com>
2024-01-02 02:44:59 +0900

    refactor: JpaRepository 사용하는 것으로 변경

--- moodtracker/src/main/java/econovation/moodtracker/repository/UserRepository.java
+++ moodtracker/src/main/java/econovation/moodtracker/repository/UserRepository.java
@@ -3,24 +3,13 @@ package econovation.moodtracker.repository;
 import econovation.moodtracker.domain.user.User;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
-public class UserRepository {
-
-    private final EntityManager em;
-
-    public void save(User user){
-        em.persist(user);
-    }
-
-    public List<User> findByUserId(String userId) {
-        return em.createQuery("select m from User m where m.userId = :userId",
-                        User.class)
-                .setParameter("userId", userId)
-                .getResultList();
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByUserId(String userId);
 }
