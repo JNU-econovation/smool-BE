af872d9c1021d55e002b1450a4490d0c452e127c
박민규 <pmq0101@naver.com>
2024-01-02 14:42:59 +0900

    feat: 날짜로 캘린더로그 찾기 기능 추가

--- moodtracker/src/main/java/econovation/moodtracker/service/CalendarLogService.java
+++ moodtracker/src/main/java/econovation/moodtracker/service/CalendarLogService.java
@@ -1,10 +1,14 @@
 package econovation.moodtracker.service;
 
 import econovation.moodtracker.domain.calendarLog.CalendarLog;
+import econovation.moodtracker.domain.emotion.Emotion;
 import econovation.moodtracker.repository.CalendarLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Transactional
@@ -18,4 +22,7 @@ public class CalendarLogService {
         return calendarLog.getId();
     }
 
+    public List<CalendarLog> findCalendarLogs(LocalDate localDate){
+        return calendarLogRepository.findCalendarLogsByDate(localDate);
+    }
 }
