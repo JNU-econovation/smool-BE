ece2f8f182134c7b01c1b0def603456b91fce2aa
박민규 <pmq0101@naver.com>
2024-01-03 01:50:03 +0900

    feat: 캘린더로그로 감정 찾기 기능 추가

--- moodtracker/src/main/java/econovation/moodtracker/repository/EmotionRepository.java
+++ moodtracker/src/main/java/econovation/moodtracker/repository/EmotionRepository.java
@@ -1,9 +1,15 @@
 package econovation.moodtracker.repository;
 
+import econovation.moodtracker.domain.calendarLog.CalendarLog;
+import econovation.moodtracker.domain.calendarLog.DTO.CalendarLogDTO;
 import econovation.moodtracker.domain.emotion.Emotion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EmotionRepository extends JpaRepository<Emotion, Long> {
+    Optional<Emotion> findEmotionByCalendarLog(CalendarLog calendarLog);
 }
