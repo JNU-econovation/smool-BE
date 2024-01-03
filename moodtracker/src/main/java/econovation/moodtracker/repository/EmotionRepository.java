b402d5e6d7f89f688565931a06f39d6cc948fc59
박민규 <pmq0101@naver.com>
2024-01-04 02:07:26 +0900

    refactor: CalendarLog 엔티티 삭제로 메서드 삭제

--- moodtracker/src/main/java/econovation/moodtracker/repository/EmotionRepository.java
+++ moodtracker/src/main/java/econovation/moodtracker/repository/EmotionRepository.java
@@ -1,7 +1,5 @@
 package econovation.moodtracker.repository;
 
-import econovation.moodtracker.domain.calendarLog.CalendarLog;
-import econovation.moodtracker.domain.calendarLog.DTO.CalendarLogDTO;
 import econovation.moodtracker.domain.emotion.Emotion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +9,4 @@ import java.util.Optional;
 
 @Repository
 public interface EmotionRepository extends JpaRepository<Emotion, Long> {
-    Optional<Emotion> findEmotionByCalendarLog(CalendarLog calendarLog);
 }
