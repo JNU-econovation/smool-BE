d16ae558d844734a02b11d6aa3a1d64e8806049e
박민규 <pmq0101@naver.com>
2024-01-05 23:05:35 +0900

    feat: List null 오류나서 기본값 추가

--- moodtracker/src/main/java/econovation/moodtracker/domain/emotion/Emotion.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/emotion/Emotion.java
@@ -13,12 +13,14 @@ import static jakarta.persistence.FetchType.*;
 @Entity
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Emotion {
     @Id
     @GeneratedValue
     @Column(name = "emotion_id")
     private Long id;
-
+    @Builder.Default
     @OneToMany(mappedBy = "emotion", fetch = LAZY)
     private List<Diary> diaries = new ArrayList<>();
     private Integer happiness;
@@ -31,14 +33,5 @@ public class Emotion {
 //        this.calendarLog = calendarLog;
 //        calendarLog.setEmotion(this);
 //    }
-    @Builder
-    public Emotion(Long id, List<Diary> diaries, Integer happiness, Integer sadness, Integer anxiety, Integer stress, Integer sleepTime) {
-        this.id = id;
-        this.diaries = diaries;
-        this.happiness = happiness;
-        this.sadness = sadness;
-        this.anxiety = anxiety;
-        this.stress = stress;
-        this.sleepTime = sleepTime;
-    }
+
 }
