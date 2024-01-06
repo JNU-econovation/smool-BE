e8b37a801f9e3af196856b3bdb86db25194d68c8
박민규 <pmq0101@naver.com>
2024-01-06 16:48:59 +0900

    refactor: UserService 파라미터 변경 Entity -> DTO

--- moodtracker/src/main/java/econovation/moodtracker/service/UserService.java
+++ moodtracker/src/main/java/econovation/moodtracker/service/UserService.java
@@ -1,5 +1,6 @@
 package econovation.moodtracker.service;
 
+import econovation.moodtracker.domain.user.DTO.Request.UserCreateRequestDTO;
 import econovation.moodtracker.domain.user.User;
 import econovation.moodtracker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,14 +18,17 @@ public class UserService {
     private final UserRepository userRepository;
 
     //회원 가입
-    public Long join(User user){
-        validateDuplicateUser(user);
+    public Long join(UserCreateRequestDTO userCreateRequestDTO){
+        User user = User.builder()
+                .userId(userCreateRequestDTO.getUserId())
+                .password(userCreateRequestDTO.getPassword())
+                .build();
         userRepository.save(user);
         return user.getId();
     }
-
-    private void validateDuplicateUser(User user) {
-        Optional<User> findUser = userRepository.findUserByUserId(user.getUserId());
+    //중복 회원
+    private void validateDuplicateUser(UserCreateRequestDTO userCreateRequestDTO){
+        Optional<User> findUser = userRepository.findUserByUserId(userCreateRequestDTO.getUserId());
         if(findUser.isPresent()){
             throw new IllegalStateException("이미 존재하는 아이디입니다.");
         }
