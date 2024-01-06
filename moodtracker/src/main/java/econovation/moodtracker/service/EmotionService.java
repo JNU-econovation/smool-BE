ddec27ee5778ba7142a22d097ebc62c3250a84ed
박민규 <pmq0101@naver.com>
2024-01-06 15:31:44 +0900

    refactor: EmotionService 코드 변경

--- moodtracker/src/main/java/econovation/moodtracker/service/EmotionService.java
+++ moodtracker/src/main/java/econovation/moodtracker/service/EmotionService.java
@@ -1,11 +1,17 @@
 package econovation.moodtracker.service;
 
 
+import econovation.moodtracker.domain.emotion.DTO.Request.EmotionCreateRequestDTO;
+import econovation.moodtracker.domain.emotion.DTO.Response.EmotionResponseDTO;
 import econovation.moodtracker.domain.emotion.Emotion;
+import econovation.moodtracker.domain.user.User;
 import econovation.moodtracker.repository.EmotionRepository;
+import econovation.moodtracker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 @Service
 @Transactional
@@ -13,10 +19,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class EmotionService {
 
     private final EmotionRepository emotionRepository;
+    private final UserRepository userRepository;
 
-    public Long join(Emotion emotion){
+    public EmotionResponseDTO join(EmotionCreateRequestDTO emotionCreateRequestDTO){
+
+        User user = userRepository.findById(emotionCreateRequestDTO.getUserPK())
+                .orElseThrow(()->new IllegalArgumentException("아이디 없음"));
+        Emotion emotion = Emotion.builder()
+                .happiness(emotionCreateRequestDTO.getHappiness())
+                .sadness(emotionCreateRequestDTO.getSadness())
+                .anxiety(emotionCreateRequestDTO.getAnxiety())
+                .stress(emotionCreateRequestDTO.getStress())
+                .sleepTime(emotionCreateRequestDTO.getSleepTime())
+                .diaries(new ArrayList<>())
+                .build();
         emotionRepository.save(emotion);
-        return emotion.getId();
+        return EmotionResponseDTO.of(emotion);
     }
 
 }
