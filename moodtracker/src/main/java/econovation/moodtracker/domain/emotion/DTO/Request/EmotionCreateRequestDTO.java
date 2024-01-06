894e3956f19ba70448864dfc2df135e4fc32e3b3
박민규 <pmq0101@naver.com>
2024-01-06 15:30:50 +0900

    feat: EmotionCreateRequestDTO 생성

---
+++ moodtracker/src/main/java/econovation/moodtracker/domain/emotion/DTO/Request/EmotionCreateRequestDTO.java
@@ -0,0 +1,26 @@
+package econovation.moodtracker.domain.emotion.DTO.Request;
+
+import econovation.moodtracker.domain.diary.Diary;
+import econovation.moodtracker.domain.emotion.Emotion;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.*;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+public class EmotionCreateRequestDTO {
+    private Long userPK;
+    private Integer happiness;
+    private Integer sadness;
+    private Integer anxiety;
+    private Integer stress;
+    private Integer sleepTime;
+    //DTO -> Entity = toEntity
+    //Entity -> DTO = of
+}
