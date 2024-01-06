5e23cb763db04c5cd49c0197d295a19b729f60f6
박민규 <pmq0101@naver.com>
2024-01-07 00:46:30 +0900

    feat: EmotionUpdateRequestDTO 추가

--- moodtracker/src/main/java/econovation/moodtracker/domain/emotion/DTO/Request/EmotionUpdateRequestDTO.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/emotion/DTO/Request/EmotionUpdateRequestDTO.java
@@ -1,4 +1,22 @@
 package econovation.moodtracker.domain.emotion.DTO.Request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
 public class EmotionUpdateRequestDTO {
+    private Long emotionPK;
+    private Integer happiness;
+    private Integer sadness;
+    private Integer anxiety;
+    private Integer stress;
+    private Integer sleepTime;
+    //DTO -> Entity = toEntity
+    //Entity -> DTO = of
 }
