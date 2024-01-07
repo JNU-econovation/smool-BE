815f6578c7ae35c059b86ddd74b827b834adf753
박민규 <pmq0101@naver.com>
2024-01-08 04:30:16 +0900

    feat: 날짜로 일기 찾기 추가

--- moodtracker/src/main/java/econovation/moodtracker/repository/DiaryRepository.java
+++ moodtracker/src/main/java/econovation/moodtracker/repository/DiaryRepository.java
@@ -4,6 +4,10 @@ import econovation.moodtracker.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+    List<Diary> findAllByTimeBetweenAndUserId(LocalDateTime startTime, LocalDateTime endTime, Long userId);
 }
