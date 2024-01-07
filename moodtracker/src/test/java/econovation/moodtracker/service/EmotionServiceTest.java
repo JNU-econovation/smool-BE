5962984fefd27e04aa34ec11aedd62d329e37c17
박민규 <pmq0101@naver.com>
2024-01-07 22:30:35 +0900

    feat: 한 개 감정 저장 테스트 코드 추가

--- moodtracker/src/test/java/econovation/moodtracker/service/EmotionServiceTest.java
+++ moodtracker/src/test/java/econovation/moodtracker/service/EmotionServiceTest.java
@@ -1,10 +1,12 @@
 package econovation.moodtracker.service;
 
+import econovation.moodtracker.domain.dto.Request.DiaryCreateRequestDTO;
 import econovation.moodtracker.repository.EmotionRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,10 +19,20 @@ class EmotionServiceTest {
     @Autowired EmotionRepository emotionRepository;
 
     @Test
+    //@Rollback(value = false)
     public void 감정저장(){
         //given
+        DiaryCreateRequestDTO diaryCreateRequestDTO = DiaryCreateRequestDTO
+                .builder()
+                .happiness(1)
+                .stress(1)
+                .anxiety(1)
+                .sleepTime(1)
+                .sadness(1)
+                .build();
         //when
+        emotionService.join(diaryCreateRequestDTO);
         //then
-
+        assertEquals(emotionRepository.findAll().size(), 1);
     }
 }
