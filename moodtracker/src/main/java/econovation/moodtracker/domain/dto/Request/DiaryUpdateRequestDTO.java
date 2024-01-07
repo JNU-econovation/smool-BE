4ed3297c3e3918d5551ccf91a968d047b941ea7c
박민규 <pmq0101@naver.com>
2024-01-07 22:59:57 +0900

    feat: DiaryUpdateRequestDTO 추가

---
+++ moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/DiaryUpdateRequestDTO.java
@@ -0,0 +1,26 @@
+package econovation.moodtracker.domain.dto.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+public class DiaryUpdateRequestDTO {
+    // 일기 domain
+    private Long diaryPK;
+    private String content;
+
+    // 감정 domain
+    private Long emotionPK;
+    private Integer happiness;
+    private Integer sadness;
+    private Integer anxiety;
+    private Integer stress;
+    private Integer sleepTime;
+}
