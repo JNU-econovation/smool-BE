5ccda58fb9c3d0b830481095591058e1240d9664
박민규 <pmq0101@naver.com>
2024-01-08 04:31:01 +0900

    feat: 일기 저장할때 의존관계 설정

--- moodtracker/src/main/java/econovation/moodtracker/service/DiaryService.java
+++ moodtracker/src/main/java/econovation/moodtracker/service/DiaryService.java
@@ -1,18 +1,32 @@
 package econovation.moodtracker.service;
 
 import econovation.moodtracker.domain.Diary;
+import econovation.moodtracker.domain.Emotion;
+import econovation.moodtracker.domain.dto.Request.DiaryCreateRequestDTO;
 import econovation.moodtracker.repository.DiaryRepository;
+import econovation.moodtracker.repository.EmotionRepository;
+import econovation.moodtracker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class DiaryService {
     private final DiaryRepository diaryRepository;
+    private final UserRepository userRepository;
+    private final EmotionService emotionService;
 
-    public Long join(Diary diary){
+    public Long join(DiaryCreateRequestDTO diaryCreateRequestDTO){
+        Diary diary = Diary.builder()
+                .time(LocalDateTime.now())
+                .content(diaryCreateRequestDTO.getContent())
+                .user(userRepository.findById(diaryCreateRequestDTO.getUserPK()).get())
+                .emotion(emotionService.join(diaryCreateRequestDTO))
+                .build();
         diaryRepository.save(diary);
         return diary.getId();
     }
