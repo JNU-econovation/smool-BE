ba9cfc62cce2e9aca6f481ace6db8147c726eb3f
박민규 <pmq0101@naver.com>
2024-01-08 19:20:52 +0900

    feat: 일기 수정 함수 매개변수 diaryCreateRequestDTO 추가 및 PK값으로 감정 찾는 함수 추가

--- moodtracker/src/main/java/econovation/moodtracker/service/EmotionService.java
+++ moodtracker/src/main/java/econovation/moodtracker/service/EmotionService.java
@@ -1,15 +1,20 @@
 package econovation.moodtracker.service;
 
 
+import econovation.moodtracker.domain.Diary;
 import econovation.moodtracker.domain.dto.Request.DiaryCreateRequestDTO;
 import econovation.moodtracker.domain.Emotion;
 import econovation.moodtracker.domain.dto.Request.DiaryUpdateRequestDTO;
+import econovation.moodtracker.repository.DiaryRepository;
 import econovation.moodtracker.repository.EmotionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,7 +22,7 @@ import java.util.ArrayList;
 public class EmotionService {
 
     private final EmotionRepository emotionRepository;
-
+    private final DiaryRepository diaryRepository;
     public Emotion join(DiaryCreateRequestDTO diaryCreateRequestDTO){
 
         Emotion emotion = Emotion.builder()
@@ -37,7 +42,39 @@ public class EmotionService {
     }
 
     public Emotion updateEmotion(DiaryCreateRequestDTO diaryCreateRequestDTO){
-        return emotionRepository.findById(diaryCreateRequestDTO.getEmotionPK())
+        LocalDate localDate = diaryCreateRequestDTO.getLocalDate();
+        LocalDateTime startTime = LocalDateTime.now()
+                .withYear(localDate.getYear())
+                .withMonth(localDate.getMonthValue())
+                .withDayOfMonth(localDate.getDayOfMonth())
+                .withHour(0)
+                .withMinute(0);
+        LocalDateTime endTime = startTime
+                .withHour(11)
+                .withMinute(59)
+                .withSecond(59);
+
+        List<Diary> diaries = diaryRepository.findAllByTimeBetweenAndUserId(startTime, endTime, diaryCreateRequestDTO.getUserPK());
+        Emotion emotion;
+//        if (diaries.isEmpty()){
+//            emotion = this.join(diaryCreateRequestDTO);
+//        }
+//        else {
+//            Long emotionPK = diaries.get(0).getEmotion().getId();
+//            emotion = emotionRepository.findById(emotionPK).get();
+//            emotion.update(diaryCreateRequestDTO.getHappiness(), diaryCreateRequestDTO.getGloom(),
+//                    diaryCreateRequestDTO.getAnxiety(), diaryCreateRequestDTO.getStress(),
+//                    diaryCreateRequestDTO.getSleep());
+            emotion = diaries.get(0).getEmotion();
+            emotion.update(diaryCreateRequestDTO.getHappiness(), diaryCreateRequestDTO.getGloom(),
+                    diaryCreateRequestDTO.getAnxiety(), diaryCreateRequestDTO.getStress(),
+                    diaryCreateRequestDTO.getSleep());
+//        }
+        return emotion;
+    }
+
+    public Emotion getEmotion(Long emotionPK){
+        return emotionRepository.findById(emotionPK)
                 .orElseThrow(() -> new NullPointerException("감정이 없어용"));
     }
 }
