77d53e80d2bdf2b36d71c975420b3d1da93e91dc
박민규 <pmq0101@naver.com>
2024-01-10 21:22:16 +0900

    feat: 회원가입 기능 추가

--- moodtracker/src/main/java/econovation/moodtracker/controller/UserController.java
+++ moodtracker/src/main/java/econovation/moodtracker/controller/UserController.java
@@ -1,11 +1,23 @@
 package econovation.moodtracker.controller;
 
+import econovation.moodtracker.domain.User;
+import econovation.moodtracker.domain.dto.Request.UserCreateRequestDTO;
 import econovation.moodtracker.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/users/join")
+    public ResponseEntity<Boolean> join(@RequestBody UserCreateRequestDTO userCreateRequestDTO){
+        userService.join(userCreateRequestDTO);
+        return new ResponseEntity<>(true, HttpStatus.CREATED);
+    }
 }
