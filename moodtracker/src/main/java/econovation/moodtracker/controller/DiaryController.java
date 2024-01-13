da2d1173ad7e282e45f9ea5e08933c852367c8c6
박민규 <pmq0101@naver.com>
2024-01-14 02:21:26 +0900

    feat: 캘린더 화면 컨트롤러 추가

--- moodtracker/src/main/java/econovation/moodtracker/controller/DiaryController.java
+++ moodtracker/src/main/java/econovation/moodtracker/controller/DiaryController.java
@@ -1,5 +1,6 @@
 package econovation.moodtracker.controller;
 
+import econovation.moodtracker.domain.dto.Response.CalendarResponseDTO;
 import econovation.moodtracker.domain.dto.Response.DiaryResponseDTO;
 import econovation.moodtracker.service.DiaryService;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
 @RequiredArgsConstructor
 public class DiaryController {
     private final DiaryService diaryService;
+    @GetMapping("/calendar/{date}")
+    public ResponseEntity<CalendarResponseDTO> findCalendar(Long userPK, @PathVariable("date")LocalDate endDate){
+        CalendarResponseDTO calendarResponseDTO = diaryService.findCalendar(endDate, userPK);
+        return new ResponseEntity<>(calendarResponseDTO, HttpStatus.OK);
+    }
+
     @GetMapping("/diaries/{id}")
     public ResponseEntity<DiaryResponseDTO> findDiary(@PathVariable("id") Long diaryPK) {
         DiaryResponseDTO diaryResponseDTO = diaryService.findOneDiary(diaryPK);
