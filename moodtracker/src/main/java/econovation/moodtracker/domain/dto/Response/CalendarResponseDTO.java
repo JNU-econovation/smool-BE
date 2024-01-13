86bb31d35a8ac5620631d870ad7658ccf8fa9bf3
박민규 <pmq0101@naver.com>
2024-01-14 02:19:37 +0900

    캘린더 전체 화면 DTO 함수 추가

--- moodtracker/src/main/java/econovation/moodtracker/domain/dto/Response/CalendarResponseDTO.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/dto/Response/CalendarResponseDTO.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static lombok.AccessLevel.PROTECTED;
@@ -15,4 +18,15 @@ import static lombok.AccessLevel.PROTECTED;
 @AllArgsConstructor
 public class CalendarResponseDTO {
     private List<CalendarDateDTO> existDates;
+
+    public static CalendarResponseDTO of(LocalDate endDate, List<Boolean> tmp){
+        List<CalendarDateDTO> existDates = new ArrayList<>();
+        LocalDate startTime = endDate.withDayOfMonth(1);
+        for(int i=0;i<tmp.size();i++){
+            existDates.add(CalendarDateDTO.of(startTime.plusDays(i), tmp.get(i)));
+        }
+        return CalendarResponseDTO.builder()
+                .existDates(existDates)
+                .build();
+    }
 }
