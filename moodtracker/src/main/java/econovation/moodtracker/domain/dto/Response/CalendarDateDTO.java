f35ee04b05495610f2c399bb8e842454f409de4e
박민규 <pmq0101@naver.com>
2024-01-14 02:18:49 +0900

    캘린더 날짜 하루 DTO 함수 추가

--- moodtracker/src/main/java/econovation/moodtracker/domain/dto/Response/CalendarDateDTO.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/dto/Response/CalendarDateDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
@@ -12,10 +14,12 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 public class CalendarDateDTO {
+    private LocalDate localDate;
     private boolean isExist;
 
-    public static CalendarDateDTO of(boolean isExist){
+    public static CalendarDateDTO of(LocalDate localDate, boolean isExist){
         return CalendarDateDTO.builder()
+                .localDate(localDate)
                 .isExist(isExist)
                 .build();
     }
