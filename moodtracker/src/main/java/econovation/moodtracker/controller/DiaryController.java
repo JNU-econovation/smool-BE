6dcbcacdda2a02080f753c7dcd7e73710272d70c
박민규 <pmq0101@naver.com>
2024-01-14 16:27:33 +0900

    feat: 일기 로그 조회 컨트롤러 추가, datetimeformat 추가

--- moodtracker/src/main/java/econovation/moodtracker/controller/DiaryController.java
+++ moodtracker/src/main/java/econovation/moodtracker/controller/DiaryController.java
@@ -5,6 +5,7 @@ import econovation.moodtracker.domain.dto.Response.DiaryLogResponseDTO;
 import econovation.moodtracker.domain.dto.Response.DiaryResponseDTO;
 import econovation.moodtracker.service.DiaryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +17,15 @@ import java.time.LocalDate;
 public class DiaryController {
     private final DiaryService diaryService;
     @GetMapping("/calendar/{dates}")
-    public ResponseEntity<CalendarResponseDTO> findCalendar(Long userPK, @PathVariable("dates")LocalDate endDate){
+    public ResponseEntity<CalendarResponseDTO> findCalendar(Long userPK, @DateTimeFormat(pattern = "yyyy-MM-dd") @PathVariable("dates")LocalDate endDate){
         CalendarResponseDTO calendarResponseDTO = diaryService.findCalendar(endDate, userPK);
         return new ResponseEntity<>(calendarResponseDTO, HttpStatus.OK);
     }
-    
+    @GetMapping("/calendar/date/{date}")
+    public ResponseEntity<DiaryLogResponseDTO> findDiaryLog (Long userPK, @DateTimeFormat(pattern = "yyyy-MM-dd") @PathVariable("date")LocalDate date){
+        DiaryLogResponseDTO diaryLogResponseDTO = diaryService.findAllDiaryLog(date, userPK);
+        return new ResponseEntity<>(diaryLogResponseDTO, HttpStatus.OK);
+    }
     @GetMapping("/diaries/{id}")
     public ResponseEntity<DiaryResponseDTO> findDiary(@PathVariable("id") Long diaryPK) {
         DiaryResponseDTO diaryResponseDTO = diaryService.findOneDiary(diaryPK);
