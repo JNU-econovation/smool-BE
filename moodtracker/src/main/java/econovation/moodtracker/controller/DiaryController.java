dc98e0d7b4fb26a4ca6a14db6062d7ce16f3c275
박민규 <pmq0101@naver.com>
2024-01-14 15:14:42 +0900

    refactor: 캘린더 화면 uri 변경

--- moodtracker/src/main/java/econovation/moodtracker/controller/DiaryController.java
+++ moodtracker/src/main/java/econovation/moodtracker/controller/DiaryController.java
@@ -1,6 +1,7 @@
 package econovation.moodtracker.controller;
 
 import econovation.moodtracker.domain.dto.Response.CalendarResponseDTO;
+import econovation.moodtracker.domain.dto.Response.DiaryLogResponseDTO;
 import econovation.moodtracker.domain.dto.Response.DiaryResponseDTO;
 import econovation.moodtracker.service.DiaryService;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +15,12 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class DiaryController {
     private final DiaryService diaryService;
-    @GetMapping("/calendar/{date}")
-    public ResponseEntity<CalendarResponseDTO> findCalendar(Long userPK, @PathVariable("date")LocalDate endDate){
+    @GetMapping("/calendar/{dates}")
+    public ResponseEntity<CalendarResponseDTO> findCalendar(Long userPK, @PathVariable("dates")LocalDate endDate){
         CalendarResponseDTO calendarResponseDTO = diaryService.findCalendar(endDate, userPK);
         return new ResponseEntity<>(calendarResponseDTO, HttpStatus.OK);
     }
-
+    
     @GetMapping("/diaries/{id}")
     public ResponseEntity<DiaryResponseDTO> findDiary(@PathVariable("id") Long diaryPK) {
         DiaryResponseDTO diaryResponseDTO = diaryService.findOneDiary(diaryPK);
