b7d2b470c368f5a66d3615d4ea57de8e140558db
박민규 <pmq0101@naver.com>
2024-01-19 00:18:51 +0900

    feat: 비밀번호 조건 추가

--- moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/UserCreateRequestDTO.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/UserCreateRequestDTO.java
@@ -2,6 +2,7 @@ package econovation.moodtracker.domain.dto.Request;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +18,8 @@ public class UserCreateRequestDTO {
     @NotBlank(message = "아이디를 입력해주세요.")
     private String userId;
     @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^(?=.[a-zA-Z])(?=.[0-9])(?=.[#?!@$%^&-]).{8,}$"
+            , message = "비밀번호는 8자 이상, 숫자, 영문 대/소문자, 특수문자를 조합해주세요.")
     private String password;
     @NotBlank(message = "비밀번호 확인을 입력해주세요.")
     private String checkPassword;
