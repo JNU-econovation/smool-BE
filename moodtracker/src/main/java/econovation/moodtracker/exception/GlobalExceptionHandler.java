4c8fce59a51833a9c21d247790807939560567e1
박민규 <pmq0101@naver.com>
2024-01-18 22:57:11 +0900

    feat: valid 에러 함수 추가

--- moodtracker/src/main/java/econovation/moodtracker/exception/GlobalExceptionHandler.java
+++ moodtracker/src/main/java/econovation/moodtracker/exception/GlobalExceptionHandler.java
@@ -2,13 +2,24 @@ package econovation.moodtracker.exception;
 
 import econovation.moodtracker.domain.dto.common.ApiResponse;
 import econovation.moodtracker.domain.dto.common.ApiResponseGenerator;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode httpStatusCode, WebRequest request) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return new ResponseEntity<>(ApiResponseGenerator.error(errorMessage), httpStatusCode);
+    }
     @ExceptionHandler(DiaryNotFoundException.class)
     public ApiResponse.Result<?> handleDiaryNotFoundException(Exception e){
         return ApiResponseGenerator.error(e.getMessage());
