84c3781a139b984613464bdfa81595169fcee19c
박민규 <pmq0101@naver.com>
2024-01-18 23:45:40 +0900

    feat: 에러 메세지 추가

--- moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/UserLoginRequestDTO.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/UserLoginRequestDTO.java
@@ -1,5 +1,6 @@
 package econovation.moodtracker.domain.dto.Request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +14,8 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 public class UserLoginRequestDTO{
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String userId;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
