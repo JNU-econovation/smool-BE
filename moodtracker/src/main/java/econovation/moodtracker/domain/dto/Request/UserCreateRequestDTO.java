491ff571a1de9e07f1d4d3abd44a1b09abbe8f65
박민규 <pmq0101@naver.com>
2024-01-18 23:57:46 +0900

    feat: 비밀번호 확인란 추가

--- moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/UserCreateRequestDTO.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/UserCreateRequestDTO.java
@@ -18,4 +18,6 @@ public class UserCreateRequestDTO {
     private String userId;
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+    @NotBlank(message = "비밀번호 확인을 입력해주세요.")
+    private String checkPassword;
 }
