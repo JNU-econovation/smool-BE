b7baa00c139f77f1a137d1047978f747def63e38
박민규 <pmq0101@naver.com>
2024-01-19 10:10:31 +0900

    refactor: 정규표현식 수정

--- moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/UserCreateRequestDTO.java
+++ moodtracker/src/main/java/econovation/moodtracker/domain/dto/Request/UserCreateRequestDTO.java
@@ -18,7 +18,7 @@ public class UserCreateRequestDTO {
     @NotBlank(message = "아이디를 입력해주세요.")
     private String userId;
     @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.[a-zA-Z])(?=.[0-9])(?=.[#?!@$%^&-]).{8,}$"
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[#?!@\\$%^&-]).{8,}$"
             , message = "비밀번호는 8자 이상, 숫자, 영문 대/소문자, 특수문자를 조합해주세요.")
     private String password;
     @NotBlank(message = "비밀번호 확인을 입력해주세요.")
